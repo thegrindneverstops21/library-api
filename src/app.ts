ac24767b03d3c4bc8282dbaab9f7087abc4e128d
thegrindneverstops21 <ndlovusamjunior21@gmail.com>
2026-09-20 23:26:27 +0200

    add: wire books in express application

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Application } from "express";
 import { logger } from "./middleware/logger";
 
+
 const app: Application = express();
 
 app.use(express.json());
 app.use(logger);
 
 app.use("/authors", require("./routes/authorRoutes"));
+app.use("/books", require("./routes/bookRoutes"));  
 
 app.get("/", (_req, res) => {
     res.status(200).json({ message: "Library API is running"});
