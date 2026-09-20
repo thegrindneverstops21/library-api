61d50df58004dad9686394f455f14226268fb3e0
thegrindneverstops21 <ndlovusamjunior21@gmail.com>
2026-09-20 23:23:14 +0200

    add: validation input for authors

--- src/routes/authorRoutes.ts
+++ src/routes/authorRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express"
 import { createAuthor, deleteAuthor, getAuthorById, getAuthors, updateAuthor } from "../controllers/authorController";
+import { validateAuthorInput } from "../middleware/validation";
 
 const router = Router();
 
-router.post("/", createAuthor);
+router.post("/", validateAuthorInput, createAuthor);
 router.get("/", getAuthors);
 router.get("/:id", getAuthorById);
-router.put("/:id", updateAuthor);
+router.put("/:id", validateAuthorInput, updateAuthor);
 router.delete("/:id", deleteAuthor);
 
 export default router;
