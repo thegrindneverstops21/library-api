9e8cdd52ee7cc5da188fb9c65a4f48a951f41110
thegrindneverstops21 <ndlovusamjunior21@gmail.com>
2026-09-20 23:21:56 +0200

    feat: set up book routing

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,1 +1,13 @@
-const router = Rou
+import { Router } from "express";
+import { validateBookInput } from "../middleware/validation";
+import { createBook, deleteBook, getBookById, getBooks, updateBook } from "../controllers/bookController";
+
+const router = Router();
+
+router.post("/", validateBookInput, createBook);
+router.get("/", getBooks);
+router.get("/:id", getBookById);
+router.put("/:id", validateBookInput, updateBook);  
+router.delete("/:id", deleteBook);
+
+export default router;
