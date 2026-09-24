17e37391a6eea1dbe03c7ed364cbeb49d4a51893
thegrindneverstops21 <ndlovusamjunior21@gmail.com>
2026-09-24 16:33:32 +0200

    feat: add author book lookup to return books belonging to the author

--- src/controllers/authorController.ts
+++ src/controllers/authorController.ts
@@ -1,7 +1,9 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { Author } from "../types";
+import { books } from "../data/books";
 import { authors } from "../data/authors";
 import { v4 as uuidv4 } from "uuid";
+import { NotFoundError } from "../errors/AppErrors";
 
 export function createAuthor(req: Request, res: Response): void {
     const { name, bio, birthYear } = req.body;
@@ -21,22 +23,22 @@ export function getAuthors(req: Request, res: Response): void {
     res.status(200).json(authors);
 }
 
-export function getAuthorById(req: Request, res: Response): void {
+export function getAuthorById(req: Request, res: Response, next: NextFunction): void {
     const author = authors.find((a) => a.id === req.params.id);
 
     if(!author) {
-        res.status(404).json({ message: "Author not found" });
+        next(new NotFoundError("Author not found"));
         return;
     }
 
     res.status(200).json(author);
 }
 
-export function updateAuthor(req: Request, res: Response): void {
+export function updateAuthor(req: Request, res: Response, next: NextFunction): void {
     const author = authors.find((a) => a.id === req.params.id);
 
     if(!author) {
-        res.status(404).json({ message: "Author not found" });
+        next(new NotFoundError("Author not found"));
         return;
     }
 
@@ -48,14 +50,26 @@ export function updateAuthor(req: Request, res: Response): void {
     res.status(200).json(author);
 }
 
-export function deleteAuthor(req: Request, res: Response): void {
+export function deleteAuthor(req: Request, res: Response, next: NextFunction): void {
     const index = authors.findIndex((a) => a.id === req.params.id);
 
     if(index === -1) {
-        res.status(404).json({ message: "Author not found" });
+        next(new NotFoundError("Author not found"));
         return;
     }
 
     authors.splice(index, 1);
     res.status(204).send();
+}
+
+export function getBooksByAuthor(req: Request, res: Response, next: NextFunction): void {
+    const author = authors.find((a) => a.id === req.params.id);
+
+    if(!author) {
+        next(new NotFoundError("Author not found"));
+        return;
+    }
+
+    const authorBooks = books.filter((b) => b.authorId === author.id);
+    res.status(200).json(authorBooks);
 }
