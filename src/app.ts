1b121e975d451a72a51c4ad285b224f1b6ea8a33
thegrindneverstops21 <ndlovusamjunior21@gmail.com>
2026-09-24 16:36:40 +0200

    fix: use default router import and error handlers

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
 import express, { Application } from "express";
 import { logger } from "./middleware/logger";
-
+import authorRoutes from "./routes/authorRoutes";
+import bookRoutes from "./routes/bookRoutes";
+import { errorHandler, notFoundHandler } from "./middleware/errorHandler";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(logger);
 
-app.use("/authors", require("./routes/authorRoutes"));
-app.use("/books", require("./routes/bookRoutes"));  
+app.use("/authors", authorRoutes);
+app.use("/books", bookRoutes);
 
 app.get("/", (_req, res) => {
     res.status(200).json({ message: "Library API is running"});
 });
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 export default app;
 
